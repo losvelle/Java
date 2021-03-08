d1ce027688178e63eacc0b934345c0907f5411cc
Carlos Velez <59077110+losvelle@users.noreply.github.com>
2021-03-08 00:54:09 -0500

    Mod3 Wk2 Hw2

---
+++ Mod3_WK2_HW2/src/main/java/com/SE/wk2hw2/Mod3Week2Hw2Application.java
@@ -0,0 +1,13 @@
+package com.SE.wk2hw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod3Week2Hw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mod3Week2Hw2Application.class, args);
+	}
+
+}
---
+++ Mod3_WK2_HW2/src/test/java/com/SE/wk2hw3/Mod3Week2Hw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SE.wk2hw3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod3Week2Hw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
